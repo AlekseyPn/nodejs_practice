3594ba609ea94522a5bce4588cfecd02b196398c
fargustian <aleksfers@icloud.com>
2020-04-05 18:03:27 +0300

    Implement routes and edit templates for routes

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const exhbs = require("./config/hbs-registration");
+const homeRoutes = require("./routes/home");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
 
 const app = express();
 exhbs.registration(app);
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({extended: true}));
+
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server is running: ", `http://localhost:${PORT}`);
-});
-
-app.get("/", (req, res) => {
-    res.render("index")
-});
-
-app.get("/about", (req, res) => {
-    res.render("about")
+  console.log("Server is running: ", `http://localhost:${PORT}`);
 });
