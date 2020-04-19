a84f67f02570f3b10f3daa1794c95d62361f1ad0
fargustian <aleksfers@icloud.com>
2020-04-19 17:33:48 +0300

    Start implement user session and registration

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
 const homeRoutes = require("./routes/home");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
 const cartRoutes = require("./routes/cart");
 const ordersRoutes = require("./routes/orders");
+const authRoutes = require("./routes/auth");
 const path = require("path");
 const db = require("./config/database");
 const User = require("./models/user");
+const varMiddleware = require("./middleware/variables");
 
 const app = express();
 require("./config/hbs-registration").registration(app);
@@ -24,12 +27,18 @@ app.use(async (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
+app.use(session({
+  secret: "secret Key Value",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(varMiddleware);
 app.use("/", homeRoutes);
 app.use("/add", addRoutes);
 app.use("/courses", coursesRoutes);
 app.use("/cart", cartRoutes);
 app.use("/orders", ordersRoutes);
-
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 8080;
 
