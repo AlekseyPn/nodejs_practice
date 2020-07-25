ac76d3e3b210cd8898a0836d991660d1f7b50322
fargustian <aleksfers@icloud.com>
2020-07-25 18:54:30 +0500

    Fix secret name and install helmet and compress

--- keys/index.js
+++ keys/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB_URI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-jkbxe.mongodb.net/shop`,
-  SESSION_SECRET: process.env.SESSION_SECRET_KEY,
+  SESSION_SECRET: process.env.SESSION_SECRET,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
   BASE_URL: process.env.BASE_URL,
